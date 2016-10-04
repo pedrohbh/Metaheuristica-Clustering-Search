57bc49cab9a717444dd2c67e6a8240905fb953d3
Eu <ajdiwjwdijdwijwd@jijdijdijfefoefoeifjoefij>
2016-10-04 18:23:08 -0300

    Primeiro Commit

---
+++ src/Grafo.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author administrador
+ */
+public class Grafo 
+{
+    
+    private String nomeInstancia; // Nome da instância do problema
+    private int dimensao;
+    private int capacidade;
+    
+    
+
+    /**
+     * @return the nomeInstancia
+     */
+    public String getNomeInstancia() {
+        return nomeInstancia;
+    }
+
+    /**
+     * @param nomeInstancia the nomeInstancia to set
+     */
+    public void setNomeInstancia(String nomeInstancia) {
+        this.nomeInstancia = nomeInstancia;
+    }
+
+    /**
+     * @return the dimensao
+     */
+    public int getDimensao() {
+        return dimensao;
+    }
+
+    /**
+     * @param dimensao the dimensao to set
+     */
+    public void setDimensao(int dimensao) {
+        this.dimensao = dimensao;
+    }
+
+    /**
+     * @return the capacidade
+     */
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    /**
+     * @param capacidade the capacidade to set
+     */
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+    
+    
+}
