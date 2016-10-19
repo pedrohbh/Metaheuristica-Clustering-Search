70035bd395ed0b74758431310383b0f89920eefc
Eu <ajdiwjwdijdwijwd@jijdijdijfefoefoeifjoefij>
2016-10-19 13:35:09 -0200

    Backup V 1.2

---
+++ src/metaheuristica/Constantes.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metaheuristica;
+
+/**
+ *
+ * @author administrador
+ */
+public class Constantes 
+{
+    public static final int MAX_NUM_CLUSTERS = 10;
+    public static final int T0 = 40000;
+    public static final double Tc =  0.01;
+    public static final double ALFA = 0.975;
+    public static final int NUM_MAX_ITER = 12000;    
+    
+}
