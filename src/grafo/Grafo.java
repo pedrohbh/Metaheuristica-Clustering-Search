cf786b58edc0690f38cd59ffc9450920e5db94be
Eu <ajdiwjwdijdwijwd@jijdijdijfefoefoeifjoefij>
2016-10-20 19:54:44 -0200

    Backup IX

--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.security.SecureRandom;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
